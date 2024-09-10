95c1b06b6d80733d01e1b83419ea0eede8ba59d0
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-09-10 13:32:23 +0500

    remove the html default lorem text.

--- src/components/header/Hero.jsx
+++ src/components/header/Hero.jsx
@@ -1,28 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/authStore";
 
 const Hero = () => {
   const navigate = useNavigate();
+  const { isAuthenticated } = useAuthStore();
 
   return (
-    <section className="bg flex flex-col justify-center px-12 gap-2">
-      <h1 className="text-7xl font-bold">First Heading</h1>
-      <p className="text-base mt-2">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, iure. <br /> ipsum dolor sit amet
-        consectetur adipisicing elit.
+    <section className="bg flex flex-col justify-center px-12 gap-4 py-16">
+      <h1 className="text-7xl font-bold w-full md:w-[65%]">Discover the Essence of Luxury</h1>
+      <p className="text-base mt-4 w-full md:w-[65%]">
+        Indulge in our exquisite collection of perfumes crafted from the finest ingredients. Whether you seek a fresh,
+        floral scent or a deep, woody allure, our fragrances offer an unforgettable experience.
       </p>
-      <div className="flex flex-col md:flex-row gap-3 mt-4">
+      <div className="flex flex-col md:flex-row gap-3 mt-6">
         <button
           onClick={() => navigate("/products")}
-          className="bg-black px-4 py-2 rounded-md border-[2px] border-black text-white hover:bg-gray-700 transition-colors"
+          className="bg-black px-6 py-3 rounded-md border-[2px] border-black text-white hover:bg-gray-700 transition-colors"
         >
-          Buy Products
+          Shop Perfumes
         </button>
-        <button
-          onClick={() => navigate("/signup")}
-          className="border-black px-4 py-2 rounded-md border-[2px] hover:bg-black hover:text-white transition"
-        >
-          Create Account
-        </button>
+        {!isAuthenticated && (
+          <button
+            onClick={() => navigate("/signup")}
+            className="border-black px-6 py-3 rounded-md border-[2px] hover:bg-black hover:text-white transition"
+          >
+            Create Account
+          </button>
+        )}
       </div>
     </section>
   );
