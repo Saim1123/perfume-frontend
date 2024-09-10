1ab34e96af88239c36fa60044da63c3f9bd8d3a5
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-09-10 13:30:29 +0500

    add pages links in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div className="p-4 border-t text-center">Copyright @amazon</div>;
+  return (
+    <footer className="bg-gray-100 p-4 text-center">
+      <ul className="flex justify-center items-center space-x-6">
+        <li>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="hover:underline">
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:underline">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
