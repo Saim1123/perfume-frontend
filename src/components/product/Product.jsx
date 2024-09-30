eb09705f99521ad50675d967b1d346b51e64f6ef
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-09-30 13:22:41 +0500

    add addToCard function on the button

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,32 +1,43 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import img1 from "../../assets/product1_img1.png";
-import img2 from "../../assets/product1_img2.png";
 
-const Product = () => {
+import { useProductStore } from "../../store/productStore";
+import { useCartStore } from "../../store/cartStore";
+
+const Product = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const { isLoading, error } = useProductStore();
+  const { addToCart } = useCartStore();
 
   const handleMouseEnter = () => setIsHovered(true);
   const handleMouseLeave = () => setIsHovered(false);
 
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  if (!product || !product.images) return <p>No product found</p>;
+  const img1 = product.images[0];
+  const img2 = product.images[1];
+
   return (
     <div className="flex flex-col shadow-md text-center">
-      <Link to={`/products/${1}`} className="cursor-pointer">
+      <Link to={`/products/${product._id}`} className="cursor-pointer">
         <img
           src={isHovered ? img2 : img1}
-          alt="image"
+          alt={product.name}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         />
       </Link>
       <div className="text-sm w-full flex flex-col gap-4 border-t py-6">
         <div className="text-[12px]">
-          <p>MOTIA SAMBAC EDP</p>
-          <p>RS.6,900</p>
+          <p>{product.name}</p>
+          <p>RS.{product.price}</p>
         </div>
         <div>
           <Link
             to="/carts"
+            onClick={() => addToCart(product._id)}
             className="border px-6 py-2 bg-black text-white cursor-pointer rounded-md hover:bg-gray-700 transition"
           >
             Add to cart
