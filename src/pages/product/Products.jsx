113345649566383dfb916c25eb67ade2f5a4ee06
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-09-30 13:27:04 +0500

    improve the products page code

--- src/pages/product/Products.jsx
+++ src/pages/product/Products.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Product from "../../components/product/Product";
+import { useProductStore } from "../../store/productStore";
+import { ClipLoader } from "react-spinners";
 
 const Products = () => {
+  const { products, fetchAllProducts, isLoading, error } = useProductStore();
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, [fetchAllProducts]);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center">
+        <ClipLoader color="black" size={40} />
+      </div>
+    );
+  if (error) console.error(error);
+
   return (
     <div className="my-6">
       <h2 className="text-center my-6 text-2xl uppercase">FLORA COLLECTION</h2>
-      <div className="w-full lg:w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 items-center">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </div>
+
+      {isLoading ? (
+        <div className="w-4/5 mx-auto flex items-center justify-center mt-4">
+          <ClipLoader color="black" size={40} />
+        </div>
+      ) : (
+        <div className="w-full lg:w-4/5 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 items-center">
+          {products.map(product => (
+            <Product id={product._id} key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
